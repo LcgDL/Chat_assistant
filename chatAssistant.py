62473274f8b89662cb7a06038d87d2bc9be6ad70
Leo C_G <84103303+LcgDL@users.noreply.github.com>
2023-06-19 12:22:33 +0200

    Add files via upload

---
+++ chatAssistant.py
@@ -0,0 +1,21 @@
+import openai
+import gradio
+
+openai.api_key = "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"
+
+mess = [{"role":"system","content":"You are a Psychologist"}]
+
+def chatGPT(input):
+    mess.append({"role":"user","content": "userInput"})
+    resp = openai.ChatCompletion.create(model="gpt-3.5-turbo", mess=mess)
+
+    chatReply = resp["choices"][0]["message"]["content"]
+    mess.append({"role":"assistant", "content": chatReply})
+    return chatReply
+
+demo = gradio.Interface(fn=chatGPT,inputs="text",outputs="text",title="Your_Title")
+
+#demo.launch()
+
+#for external link
+demo.launch(share=True)
